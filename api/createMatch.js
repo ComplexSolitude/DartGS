4abe233a0ca89f301752661f8ddfb6f70aa98968
titchoj <138580334+ComplexSolitude@users.noreply.github.com>
2025-08-05 14:30:58 +0100

    Update createMatch.js

--- api/createMatch.js
+++ api/createMatch.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       {
         method: 'POST',
         headers: {
-          'Content-Type': 'text/plain;charset=utf-8',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ match_id }),
       }
