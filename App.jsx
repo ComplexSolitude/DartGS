dfa41ed9c2649b75e7c90b8601c3836362a351ae
titchoj <138580334+ComplexSolitude@users.noreply.github.com>
2025-08-05 21:03:10 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,19 +6,19 @@ import MatchStatsWrapper from './MatchStatsWrapper';
 export default function App() {
   return (
     <Router>
-        <header style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '1rem',
-            backgroundColor: '#222'
-        }}>
-            <img
-              src="/Arms.jpg"
-              alt="Logo"
-              style={{ maxHeight: '60px' }}
-            />
-        </header>
+        {/*<header style={{*/}
+        {/*    display: 'flex',*/}
+        {/*    justifyContent: 'center',*/}
+        {/*    alignItems: 'center',*/}
+        {/*    padding: '1rem',*/}
+        {/*    backgroundColor: '#222'*/}
+        {/*}}>*/}
+        {/*    <img*/}
+        {/*      src="/Arms.jpg"*/}
+        {/*      alt="Logo"*/}
+        {/*      style={{ maxHeight: '60px' }}*/}
+        {/*    />*/}
+        {/*</header>*/}
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/match/:id" element={<MatchStatsWrapper />} />
